0b16cf8b12507afc4d2fd0e07bbcd20ad6d8e1c7
mrbiaar <157238013+mrbiaar@users.noreply.github.com>
2024-06-28 21:20:44 +0200

    Archivos de JS, hola mundo y Variables
    En uno es para aprender a usar el console.log y el otro es para aprender a declarar variables y tipos en el JS, y cómo verlos con console.log() en la inspección de la página

---
+++ variables.js
@@ -0,0 +1,22 @@
+//strings
+
+var userName = "Manuel";
+userSurename = "Rubial";
+//con acento abierto es una Template literal.
+userTitle = `Knight`; 
+
+const espacio = " ";
+// {
+//let variable = varifun
+//con let, la variable solo existe dentro de las llaves
+// }
+
+console.log(userName);
+console.log(userSurename);
+console.log(userTitle);
+
+console.log(userName, userSurename, userTitle);
+
+console.log(userTitle + " " + userName + " " + userSurename);
+
+console.log(`¿Qué tal estás, ${userName}, ${userSurename});
